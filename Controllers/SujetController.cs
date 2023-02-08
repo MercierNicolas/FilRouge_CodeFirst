fdd23b443e08cf5fa369b4b70d7e61e449ec3bd0
Nicolas <mr.mercier.nicolas@gmail.com>
2023-02-08 20:13:49 +0100

    Back(Feature): Ajout Creation Sujet

---
+++ Controllers/SujetController.cs
@@ -0,0 +1,46 @@
+﻿using FilRouge_Test_CodeFirst.Data.Entity;
+using FilRouge_Test_CodeFirst.Domaine;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilRouge_Test_CodeFirst.Controllers
+{
+    public class SujetController : Controller
+    {
+        private readonly ISujetRepository sujetRepo;
+
+        public SujetController(ISujetRepository sujetAdd)
+        {
+            this.sujetRepo = sujetAdd;
+        }
+        public IActionResult Index()
+        {
+            var listeSujet= sujetRepo.GetAllSujet();
+            return View(listeSujet);
+        }
+
+        public IActionResult AddSujet()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddSujet(Sujet model)
+        {
+            var sujetAjout = new Sujet()
+            {
+                SujetName = model.SujetName,
+            };
+            sujetRepo.CreateSujet(sujetAjout);
+            return RedirectToAction("Index");
+        }
+        public IActionResult DeleteSujet(int id)
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult DeleteLevel(Level model)
+        {
+            sujetRepo.DeleteSujet(model.Id);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Domaine/ISujetRepository.cs
@@ -0,0 +1,46 @@
+﻿using FilRouge_Test_CodeFirst.Data;
+using FilRouge_Test_CodeFirst.Data.Entity;
+
+namespace FilRouge_Test_CodeFirst.Domaine
+{
+    public interface ISujetRepository
+    {
+        int CreateSujet(Sujet sujet);
+        IEnumerable<Sujet> GetAllSujet();
+        IEnumerable<Sujet> GetOneSujet(int id);
+        int DeleteSujet(int id);
+    }
+
+    public class DbSujetlRepo : ISujetRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public DbSujetlRepo(ApplicationDbContext context)
+        {
+            this._context = context;
+        }
+        public int CreateSujet(Sujet sujet)
+        {
+            _context.sujets.Add(sujet);
+            _context.SaveChanges();
+            return sujet.id;
+        }
+        public IEnumerable<Sujet> GetAllSujet()
+        {
+            return _context.sujets;
+        }
+
+        public IEnumerable<Sujet> GetOneSujet(int id)
+        {
+            return _context.sujets.Where(l => l.id == id).ToList();
+        }
+
+        public int DeleteSujet(int id)
+        {
+            var SujetlAdelete = _context.sujets.Where(l => l.id == id).ToList();
+            _context.sujets.RemoveRange(SujetlAdelete);
+            _context.SaveChanges();
+            return 0;
+        }
+
+    }
+}
