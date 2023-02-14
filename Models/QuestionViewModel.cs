a97d5c6bf9f071eb0db117bda27a397635f792b5
nicolas <mr.mercier.nicolas@gmail.com>
2023-02-14 11:54:12 +0100

    BACK(Ajout):Creation question

---
+++ Models/QuestionViewModel.cs
@@ -0,0 +1,16 @@
+﻿using FilRouge_Test_CodeFirst.Data.Entity;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace FilRouge_Test_CodeFirst.Models
+{
+    public class QuestionViewModel
+    {
+        public Question question { get; set; }
+        public IEnumerable<SelectListItem> AllLevel { get; set; }
+        public int LevelId { get; set; }
+
+        public IEnumerable<SelectListItem> AllSujet { get; set; }
+        public int sujetId { get; set; }
+
+    }
+}
